97db90c5b562a2b53cf36e958b2769166a959a5d
Nathália Rodrigues <nathaliarsantos@live.com>
2022-09-26 14:04:26 -0300

    Python parte 3

---
+++ Exercícios Parte 3/ex097.py
@@ -0,0 +1,12 @@
+print('Um print especial')
+def escreva(msg):
+    tam = len(msg) + 4
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+
+
+# Programa principal
+escreva('Nathália Rodrigues')
+escreva('Curso de Python no Youtube')
+escreva('CURSO EM VIDEO')
