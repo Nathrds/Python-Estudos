26f0226234a89593001dba7ae9a5279ba55a1fd6
Nathália Rodrigues <nathaliarsantos@live.com>
2022-09-30 18:50:56 -0300

    Python parte 3

---
+++ Exercícios Parte 3/ex113.py
@@ -0,0 +1,34 @@
+print('Funções aprofundadas em Python')
+
+
+def leiaInt(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print('\033[31mERRO! Por favor, digite um número inteiro válido.\033[m')
+            continue
+        except (KeyboardInterrupt):
+            print('\n\033[31mEntrada de dados interrompida pelo usuário.\033[m')
+            return 0
+        else:
+            return n
+
+
+def leiaFloat(msg):
+    while True:
+        try:
+            n = float(input(msg))
+        except (ValueError, TypeError):
+            print('\033[31mERRO! Por favor, digite um número real válido.\033[m')
+            continue
+        except (KeyboardInterrupt):
+            print('\n\033[31mUsuário preferiu não digitar esse número.\033[m')
+            return 0
+        else:
+            return n
+
+
+num1 = leiaInt('Digite um número Inteiro: ')
+num2 = leiaFloat('Digite um número Real: ')
+print(f'O valor inteiro digitado foi {num1} e o real foi {num2}.')
